e5b885e20191817880c641f7f473cf1b2bacdcce
bluce <liushuo_whu@163.com>
2017-08-20 22:30:26 +0800

    创建图层树

--- ng-zorro-cesium/src/app/components/layer-tree/layer-tree.component.ts
+++ ng-zorro-cesium/src/app/components/layer-tree/layer-tree.component.ts
@@ -1,13 +1,56 @@
-import { Component, OnInit } from "@angular/core";
-import { TreeModule, TreeNode } from "primeng/primeng";
+import { Component, OnInit, AfterViewInit } from "@angular/core";
+import { TreeNode } from "primeng/primeng";
+
+import { LayerService } from "../../services/layer/layer.service";
+import { CesiumService } from "../../services/cesium/cesium.service";
 
 @Component({
   selector: "qk-earth-layer-tree",
-  templateUrl: "./layer-tree.component.html",
+  template: `
+     <p-tree [value]="treeNodes" selectionMode="checkbox"
+      [(selection)]="selectedNodes"
+      [propagateSelectionUp]="false"
+      [propagateSelectionDown]="false"
+      (onNodeSelect)="treeNodeSelect($event)"
+      (onNodeUnselect)="treeNodeUnSelect($event)">
+      </p-tree>
+  `,
   styleUrls: ["./layer-tree.component.css"]
 })
-export class LayerTreeComponent implements OnInit {
-  constructor() {}
+export class LayerTreeComponent implements OnInit, AfterViewInit {
+  public treeNodes: TreeNode[];
+  public selectedNodes: TreeNode[];
+  public viewer: any;
+  constructor(
+    private layerService: LayerService,
+    private cesiumService: CesiumService
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.treeNodes = this.layerService.getLayerTreeNodes();
+  }
+  ngAfterViewInit() {
+    this.viewer = this.cesiumService.getViewer();
+  }
+
+  treeNodeSelect(event) {
+    console.log(event);
+    this.viewer.imageryLayers.addImageryProvider(
+      this.layerService.getImageryProviderByName(event.node.label)
+    );
+  }
+  treeNodeUnSelect(event) {
+    console.log(event);
+    this.viewer.imageryLayers.remove(this.getLayerByName(event.node.label));
+  }
+
+  getLayerByName(layerName: string) {
+    let layer = null;
+    this.viewer.imageryLayers._layers.forEach(el => {
+      if (el.imageryProvider.credit.text === layerName) {
+        layer = el;
+      }
+    });
+    return layer;
+  }
 }
