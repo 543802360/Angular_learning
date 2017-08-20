00edae46f06639123f50cf29afaac367899c2ea4
bluce <liushuo_whu@163.com>
2017-08-20 16:01:20 +0800

    添加图层树组件

---
+++ ng-zorro-cesium/src/app/components/layer-tree/layer-tree.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LayerTreeComponent } from './layer-tree.component';
+
+describe('LayerTreeComponent', () => {
+  let component: LayerTreeComponent;
+  let fixture: ComponentFixture<LayerTreeComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LayerTreeComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LayerTreeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ ng-zorro-cesium/src/app/components/layer-tree/layer-tree.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+import { TreeModule, TreeNode } from "primeng/primeng";
+
+@Component({
+  selector: "qk-earth-layer-tree",
+  templateUrl: "./layer-tree.component.html",
+  styleUrls: ["./layer-tree.component.css"]
+})
+export class LayerTreeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
