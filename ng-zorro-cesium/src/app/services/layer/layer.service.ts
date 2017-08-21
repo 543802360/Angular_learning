df98cc425c0beb879d4cccbd4d79620d340ccd65
bluce <liushuo_whu@163.com>
2017-08-21 22:18:14 +0800

    添加根据名称获取ImageryLayer函数

--- ng-zorro-cesium/src/app/services/layer/layer.service.ts
+++ ng-zorro-cesium/src/app/services/layer/layer.service.ts
@@ -5,16 +5,40 @@ import { CesiumService } from "../cesium/cesium.service";
 
 @Injectable()
 export class LayerService {
-
-  constructor() {
-  }
+  constructor(private cesiumService: CesiumService) {}
+  /**
+   * 获取图层树初始化节点集
+   *
+   * @returns {object[]}
+   * @memberof LayerService
+   */
   getLayerTreeNodes(): object[] {
     return LayerTreeNodes;
   }
+  /**
+   *
+   * 根据名称获取ImageryProvider
+   * @param {string} layerName
+   * @returns
+   * @memberof LayerService
+   */
   getImageryProviderByName(layerName: string) {
     return ImageryProviders[layerName];
   }
-  getLayerByName(layerName: string) {
 
+  /**
+   *
+   *
+   * @param {string} layerName
+   * @memberof LayerService
+   */
+  getImageryLayerByName(layerName: string) {
+    let layer = null;
+    this.cesiumService.getViewer().imageryLayers._layers.forEach(el => {
+      if (el.imageryProvider.credit.text === layerName) {
+        layer = el;
+      }
+    });
+    return layer;
   }
 }
