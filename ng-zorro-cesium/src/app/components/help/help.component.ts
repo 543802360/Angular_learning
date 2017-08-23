3588e1ad45e0a9e1ac4cde4402e8b9c5b82e4929
bluce <liushuo_whu@163.com>
2017-08-23 21:18:02 +0800

    add help components

---
+++ ng-zorro-cesium/src/app/components/help/help.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'qk-earth-help',
+  templateUrl: './help.component.html',
+  styleUrls: ['./help.component.css']
+})
+export class HelpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
