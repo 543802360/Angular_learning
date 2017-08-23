24d8ab6d34a0f8bd58b6766fb21dc6a643a93663
bluce <liushuo_whu@163.com>
2017-08-23 21:18:20 +0800

    add measure components

---
+++ ng-zorro-cesium/src/app/components/measure/measure.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'qk-earth-measure',
+  templateUrl: './measure.component.html',
+  styleUrls: ['./measure.component.css']
+})
+export class MeasureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
