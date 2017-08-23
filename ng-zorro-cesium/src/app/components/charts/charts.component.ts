923b252b691ed6db9db7a1f4452c74bd0c278eaa
bluce <liushuo_whu@163.com>
2017-08-23 21:30:46 +0800

    add charts component

---
+++ ng-zorro-cesium/src/app/components/charts/charts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'qk-earth-charts',
+  templateUrl: './charts.component.html',
+  styleUrls: ['./charts.component.css']
+})
+export class ChartsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
