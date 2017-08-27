2e0e658166f62ef565053fe3de536f1819ea47cd
bluce <liushuo_whu@163.com>
2017-08-27 21:44:13 +0800

    更新viewer组件

--- ng-zorro-cesium/src/app/components/viewer/viewer.component.ts
+++ ng-zorro-cesium/src/app/components/viewer/viewer.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from "@angular/core";
 import { LayerService } from "../../services/layer/layer.service";
 @Component({
   selector: "qk-earth-viewer",
-  templateUrl: "./viewer.component.html",
+  template: `
+    <qkEarthCesiumViewer [options]="options"></qkEarthCesiumViewer>
+  `,
   styleUrls: ["./viewer.component.css"]
 })
 export class ViewerComponent implements OnInit {
@@ -42,20 +44,20 @@ export class ViewerComponent implements OnInit {
       //   requestVertexNormals: false,
       //   requestWaterMask: false
       // }),
-      // imageryProvider: new Cesium.WebMapTileServiceImageryProvider({
-      //   url:
-      //     "http://{s}.tianditu.com/img_w/wmts?service=WMTS&version=1.0.0&request=GetTile&ti" +
-      //     "lematrix={TileMatrix}&layer=img&style={style}&tilerow={TileRow}&tilecol={TileCol" +
-      //     "}&tilematrixset={TileMatrixSet}&format=tiles",
-      //   layer: "img",
-      //   style: "default",
-      //   format: "tiles",
-      //   tileMatrixSetID: "w",
-      //   credit: new Cesium.Credit("天地图影像"),
-      //   subdomains: ["t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7"],
-      //   minimumLevel: 0,
-      //   maximumLevel: 19
-      // }),
+      imageryProvider: new Cesium.WebMapTileServiceImageryProvider({
+        url:
+          "http://{s}.tianditu.com/img_w/wmts?service=WMTS&version=1.0.0&request=GetTile&ti" +
+          "lematrix={TileMatrix}&layer=img&style={style}&tilerow={TileRow}&tilecol={TileCol" +
+          "}&tilematrixset={TileMatrixSet}&format=tiles",
+        layer: "img",
+        style: "default",
+        format: "tiles",
+        tileMatrixSetID: "w",
+        credit: new Cesium.Credit("天地图影像"),
+        subdomains: ["t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7"],
+        minimumLevel: 0,
+        maximumLevel: 19
+      }),
       vrButton: true
     };
   }
